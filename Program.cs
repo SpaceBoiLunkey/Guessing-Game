52d7065af5d3a5592d2305788933ff0333317eea
SpaceBoiLunkey <kalthenonbinarypal@gmail.com>
2021-08-27 14:37:57 -0500

    fixed repo

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+    Console.WriteLine("Welcome to Guessing Game!");
+    
+
+}
+
+for (int i = 4; i > 0; i--)
+{
+bool SecretNumberAsk = GuessingGame("What is the secret number?");
+if (SecretNumberAsk)
+{
+    SecretNumberAnswer("You guessed the number!");
+}
+else
+{
+    SecretNumberAnswer("You did not guess the secret number!");
+}
+
+bool GuessingGame(string question)
+{
+    Console.WriteLine($"Your guess: ");
+    string answer = Console.ReadLine();
+
+    if (answer == "42")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+};
+void SecretNumberAnswer(string message)
+{
+    Console.WriteLine($@"{message}");
+}
